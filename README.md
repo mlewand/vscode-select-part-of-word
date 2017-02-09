0d4de0ed374f2faa07e41d1e4bbac87362ea37d3
Marek Lewandowski <marek@mlewandowski.com>
2017-02-09 23:44:18 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# Select Part Of Word
+
+An extension that allows you to move selection by a word in camel-case string.
---
+++ extension.js
@@ -0,0 +1,7 @@
+var vscode = require( 'vscode' );
+
+function activate( context ) {}
+exports.activate = activate;
+
+function deactivate() {}
+exports.deactivate = deactivate;
