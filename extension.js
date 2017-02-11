5931376d6a864d531e0a48c08a4184a6b38a537c
Marek Lewandowski <marek@mlewandowski.com>
2017-02-11 02:29:51 +0100

    Fixed #4 - Added the code for selectLeft command.

--- extension.js
+++ extension.js
@@ -3,11 +3,13 @@ const vscode = require( 'vscode' ),
 
 function activate( context ) {
 	let moveRightCmd = vscode.commands.registerTextEditorCommand( 'selectPartOfWord.moveRight', commands.moveRight, commands ),
+		moveLeftCmd = vscode.commands.registerTextEditorCommand( 'selectPartOfWord.moveLeft', commands.moveLeft, commands ),
 		selectRightCmd = vscode.commands.registerTextEditorCommand( 'selectPartOfWord.selectRight', commands.selectRight, commands ),
-		moveLeftCmd = vscode.commands.registerTextEditorCommand( 'selectPartOfWord.moveLeft', commands.moveLeft, commands );
+		selectLeftCmd = vscode.commands.registerTextEditorCommand( 'selectPartOfWord.selectLeft', commands.selectLeft, commands );
 
 	context.subscriptions.push( moveRightCmd );
-	context.subscriptions.push( selectRightCmd );
 	context.subscriptions.push( moveLeftCmd );
+	context.subscriptions.push( selectRightCmd );
+	context.subscriptions.push( selectLeftCmd );
 }
 exports.activate = activate;
