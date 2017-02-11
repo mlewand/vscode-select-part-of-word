cfe26661e7c31e78b81f64a4bcfae7114d167b97
Marek Lewandowski <marek@mlewandowski.com>
2017-02-11 02:16:13 +0100

    Exposed commands.

--- extension.js
+++ extension.js
@@ -1,7 +1,13 @@
-var vscode = require( 'vscode' );
+const vscode = require( 'vscode' ),
+	commands = require( './src/commands' );
 
-function activate( context ) {}
+function activate( context ) {
+	let moveRightCmd = vscode.commands.registerTextEditorCommand( 'selectPartOfWord.moveRight', commands.moveRight, commands ),
+		selectRightCmd = vscode.commands.registerTextEditorCommand( 'selectPartOfWord.selectRight', commands.selectRight, commands ),
+		moveLeftCmd = vscode.commands.registerTextEditorCommand( 'selectPartOfWord.moveLeft', commands.moveLeft, commands );
+
+	context.subscriptions.push( moveRightCmd );
+	context.subscriptions.push( selectRightCmd );
+	context.subscriptions.push( moveLeftCmd );
+}
 exports.activate = activate;
-
-function deactivate() {}
-exports.deactivate = deactivate;
