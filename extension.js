c8d50a00e0e3675fb0ae284ec8b0621d7d3ea640
Marek Lewandowski <marek@mlewandowski.com>
2017-02-14 02:25:20 +0100

    Fixed #8 - Undo images are no longer created by selectPartOfWord.* commands.

--- extension.js
+++ extension.js
@@ -2,10 +2,10 @@ const vscode = require( 'vscode' ),
 	commands = require( './src/commands' );
 
 function activate( context ) {
-	let moveRightCmd = vscode.commands.registerTextEditorCommand( 'selectPartOfWord.moveRight', commands.moveRight, commands ),
-		moveLeftCmd = vscode.commands.registerTextEditorCommand( 'selectPartOfWord.moveLeft', commands.moveLeft, commands ),
-		selectRightCmd = vscode.commands.registerTextEditorCommand( 'selectPartOfWord.selectRight', commands.selectRight, commands ),
-		selectLeftCmd = vscode.commands.registerTextEditorCommand( 'selectPartOfWord.selectLeft', commands.selectLeft, commands );
+	let moveRightCmd = vscode.commands.registerCommand( 'selectPartOfWord.moveRight', commands.moveRight, commands ),
+		moveLeftCmd = vscode.commands.registerCommand( 'selectPartOfWord.moveLeft', commands.moveLeft, commands ),
+		selectRightCmd = vscode.commands.registerCommand( 'selectPartOfWord.selectRight', commands.selectRight, commands ),
+		selectLeftCmd = vscode.commands.registerCommand( 'selectPartOfWord.selectLeft', commands.selectLeft, commands );
 
 	context.subscriptions.push( moveRightCmd );
 	context.subscriptions.push( moveLeftCmd );
