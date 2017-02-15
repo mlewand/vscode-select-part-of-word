1fa807bf4474e300bc495d3782c726374939a995
Marek Lewandowski <marek@mlewandowski.com>
2017-02-15 23:27:15 +0100

    In the process of rewriting movePosition method.

--- src/commands.js
+++ src/commands.js
@@ -1,10 +1,6 @@
 const common = require( './common' ),
 	vscode = require( 'vscode' ),
 	CHAR_TYPE = common.CHAR_TYPE,
-	STRATEGIES = {
-		CAMEL_CASE: 1,
-		SAME_CASE: 2
-	},
 	regExpMapping = common.regExpMapping,
 	regExpExcludeMapping = common.regExpExcludeMapping;
 
@@ -104,82 +100,18 @@ module.exports = {
 	 */
 	_movePosition( doc, position, right ) {
 		let linesGenerator = this._getAheadLines( doc, position, Boolean( right ) ),
-			endPos = null,
-			curLine,
-			nextLineFeed;
+			endPos = null;
 
-		// @todo extract it to a separate method.
-		( function() {
-			// First line is special, there's multiple different checks to be done.
-			nextLineFeed = linesGenerator.next().value;
+		for ( [ curLine, textAhead ] of linesGenerator ) {
+			if ( curLine === position.line ) {
+				// First line has some special handling.
+			} else {
+				let match = textAhead.search( /[^\s]/ );
 
-			curLine = nextLineFeed[ 0 ];
-
-			let lineText = doc.lineAt( curLine ).text,
-				// The text after the selection.
-				siblingText = nextLineFeed[ 1 ],
-				previousChar = right ? lineText[ position.character - 1 ] : lineText[ position.character ],
-				lastCharType = this._getCharType( previousChar || ' ' ),
-				curCharType = this._getCharType( siblingText[ 0 ] ),
-				lineBoundaryPosition = right ? lineText.length : 0;
-
-			if ( curCharType !== lastCharType ) {
-				// Handles cases like (LTR):
-				// is^CorrectColor
-				// isC^orrectColor
-				// is ^correct
-				// is^ correct
-				let farAheadCharType = this._getCharType( right ? siblingText[ 1 ] : siblingText.substr( -2, 1 ) ),
-					moveOffset;
-
-				if ( curCharType !== farAheadCharType ) {
-					// Case 1 above, caret is before the first capitalized letter in camel case.
-					if ( right ) {
-						moveOffset = siblingText.substr( 1 ).search( regExpExcludeMapping[ farAheadCharType ] );
-
-						endPos = moveOffset === -1 ?
-							// No other characters found in this line.
-							null : // Not found... @todo: we'll be skipping to next line here.
-							// Note we're skipping first char (capitalized letter), and because of that we're adding 1.
-							position.character + moveOffset + 1;
-					} else {
-						moveOffset = siblingText.search( regExpExcludeMapping[ curCharType ] );
-
-						endPos = moveOffset === -1 ?
-							// No other characters found in this line.
-							null : // Not found... @todo: we'll be skipping to next line here.
-							// Or just subtract offset from start char position.
-							position.character - moveOffset;
-					}
-
-					if ( endPos === null && position.character !== lineBoundaryPosition ) {
-						endPos = lineBoundaryPosition;
-					}
+				if ( match !== -1 ) {
+					endPos = right ? match : textAhead.length - match;
+					break;
 				}
-			}
-
-			if ( endPos === null ) {
-				exclusionPosition = right ? siblingText.search( regExpExcludeMapping[ curCharType ] ) :
-					siblingText.search( regExpExcludeMapping[ curCharType ] );
-
-				if ( exclusionPosition !== -1 ) {
-					endPos = right ? position.character + exclusionPosition :
-						position.character - exclusionPosition - 1;
-				}
-			}
-
-			if ( endPos === null && position.character !== lineBoundaryPosition ) {
-				endPos = lineBoundaryPosition;
-			}
-		} ).call( this );
-
-		while ( endPos === null && ( nextLineFeed = linesGenerator.next().value ) ) {
-			curLine = nextLineFeed[ 0 ];
-			let lineText = nextLineFeed[ 1 ],
-				match = lineText.search( /[^\s]/ );
-
-			if ( match !== -1 ) {
-				endPos = right ? match : lineText.length - match;
 			}
 		}
 
@@ -230,7 +162,7 @@ module.exports = {
 				}
 
 				return ret;
-			}
+			};
 
 		while ( isLineValid( curLine ) ) {
 			yield [
