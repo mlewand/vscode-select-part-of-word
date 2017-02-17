652543e0eac59e9bd2d77727d85dfda30f9cc9bd
Marek Lewandowski <marek@mlewandowski.com>
2017-02-17 02:40:46 +0100

    Added tests for moveLeft multi selection support.

--- test/extension.test.js
+++ test/extension.test.js
@@ -433,6 +433,26 @@
                     assert.equal( getContent.withSelection( textEditor ), expected );
                 } );
         } );
+
+        test( 'Supports multiple selections', function() {
+            return vscode.workspace.openTextDocument( path.join( __dirname, '_fixtures', 'camelCase.txt' ) )
+                .then( ( doc ) => {
+                    return vscode.window.showTextDocument( doc );
+                } )
+                .then( textEditor => {
+                    let expected = '^thisIsACamel^CaseWord itsSuperFun   to	writeIn-CamelCase\n' +
+                        'you^ could also mix it with12345wordsToSee how it behaves with numbers';
+                    textEditor.selections = [
+                        new vscode.Selection( 0, 0, 0, 0 ),
+                        new vscode.Selection( 0, 14, 0, 14 ),
+                        new vscode.Selection( 1, 4, 1, 4 )
+                    ];
+
+                    commands.moveLeft( textEditor );
+
+                    assert.equal( getContent.withSelection( textEditor ), expected );
+                } );
+        } );
     } );
 
     suite( 'commands.selectRight', function() {
