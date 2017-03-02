d7f18042ab47f75f17467a16926be5c9b0da7647
Marek Lewandowski <marek@mlewandowski.com>
2017-03-03 00:33:55 +0100

    Fixed #24 - Make sure that moveLeft camelCase customization is not executed in other cases.

--- src/commands.js
+++ src/commands.js
@@ -171,7 +171,8 @@ module.exports = {
 			matchAdjustment = 1;
 		}
 
-		if ( !right && match !== -1 && this._getCharType( textAhead[ match ] ) === common.CHAR_TYPE.UPPER_CASE ) {
+		if ( !right && match !== -1 && this._getCharType( textAhead[ match ] ) === common.CHAR_TYPE.UPPER_CASE &&
+			curCharType === common.CHAR_TYPE.LOWER_CASE ) {
 			// Detects case like moving left with thisIsFanc^y - we want to move  the caret before
 			// uppercased letter.
 			match += 1;
